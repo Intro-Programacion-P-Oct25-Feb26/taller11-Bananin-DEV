33a5857d2077aa90ddaab235d06030146b557963
Bananin-DEV <mjvaldivieso6@utpl.edu.ec>
2026-01-08 11:58:10 -0500

    Ya casi termino el 02

--- ejercicios/Problemas/src/paquete02/Problema2.java
+++ ejercicios/Problemas/src/paquete02/Problema2.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package paquete02;
+
 import java.util.Scanner;
 
 /**
@@ -20,15 +21,44 @@ public class Problema2 {
         String[] viviendas = obtenerViviendas();
         double[][] consumos = obtenerConsumos();
         double[] consumoAnual = calcularConsumoAnual(consumos);
-        generarReporte(viviendas,consumoAnual);
+        generarReporte(viviendas, consumoAnual);
     }
-    public static String[]obtenerViviendas(){
-     Scanner entrada = new Scanner(System.in); 
-     String[]viviendas = new String[10];
-     for (int i = 0; i < viviendas.length; i++){
-         System.out.println("Ingrese el nombre de la vivienda"+(i+1)+":");
-         viviendas[i] = entrada.nextLine();
-     }
-     return viviendas;
+
+    public static String[] obtenerViviendas() {
+        Scanner entrada = new Scanner(System.in);
+        String[] viviendas = new String[10];
+        for (int i = 0; i < viviendas.length; i++) {
+            System.out.println("Ingrese el nombre de la vivienda" + (i + 1) + ":");
+            viviendas[i] = entrada.nextLine();
+        }
+        return viviendas;
     }
+
+    public static double[][] obtenerConsumos() {
+        Scanner entrada = new Scanner(System.in);
+        double[][] consumo = new double[10][12];
+        for (int i = 0; i < consumo.length; i++) {
+            System.out.println("Ingresar consumo de la vivienda" + (i + 1));
+            for (int j = 0; j < consumo[i].length; j++) {
+                System.out.print("Mes" + (j + 1) + ":");
+                consumo[i][j] = entrada.nextDouble();
+            }
+        }
+        return consumo;
+    }
+
+    public static double[] calcularConsumoAnual(double[][] consumos) {
+        
+        double[] totalAnual = new double[consumos.length];
+
+        for (int i = 0; i < consumos.length; i++) {
+            double suma = 0;
+            for (int j = 0; j < consumos[i].length; j++) {
+                suma += consumos[i][j];
+            }
+            totalAnual[i] = suma;
+        }
+        return totalAnual;
+    }
+    
 }
