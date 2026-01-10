73ea3ead9a5bcabac843d3666e1a07610f81c816
Bananin-DEV <mjvaldivieso6@utpl.edu.ec>
2026-01-09 21:22:29 -0500

    Cambios finales

--- ejercicios/Problemas/src/paquete04/Problema4.java
+++ ejercicios/Problemas/src/paquete04/Problema4.java
@@ -5,9 +5,10 @@
  */
 package paquete04;
 
-/**
- *
- * @author reroes
+import java.util.Scanner;
+
+/*
+         * @author reroes
  */
 public class Problema4 {
 
@@ -16,6 +17,73 @@ public class Problema4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        String nombre;
+        String cedula;
+        String mensaje = "";
+
+        System.out.printf("Seleccione una opcion:\n1. Calcular el valor"
+                + " de planilla de luz\n2. Calcular el valor del predio\n");
+        opcion = entrada.nextInt();
+        entrada.nextLine();
+
+        System.out.println("Ingrese el nombre del cliente: ");
+        nombre = entrada.nextLine();
+        System.out.println("Ingrese la cedula del cliente: ");
+        cedula = entrada.nextLine();
+
+        if (opcion == 1) {
+            calcularValorLuz(nombre, cedula);
+        } else if (opcion == 2) {
+            calcularPredio(nombre, cedula);
+        } else {
+            System.out.println("Opcion no valida");
+        }
     }
-    
+
+    public static void calcularValorLuz(String nombre, String cedula) {
+        Scanner entrada = new Scanner(System.in);
+        double valorKilowatio;
+        double numeroKilowatios;
+        double total;
+        String mensaje = "";
+
+        System.out.print("Ingrese el valor del kilowatio: ");
+        valorKilowatio = entrada.nextDouble();
+        System.out.print("Ingrese el numero de kilowatios del mes: ");
+        numeroKilowatios = entrada.nextDouble();
+
+        total = valorKilowatio * numeroKilowatios;
+
+        mensaje = mensaje + String.format(
+                "El cliente %s con cedula %s debe cancelar el valor de $%.2f%n",
+                nombre, 
+                cedula, 
+                total);
+
+        System.out.print(mensaje);
+    }
+
+    public static void calcularPredio(String nombre, String cedula) {
+        Scanner entrada = new Scanner(System.in);
+        double valorInmueble;
+        double valorPredio;
+        String mensaje = "";
+
+        System.out.print("Ingrese el valor del inmueble: ");
+        valorInmueble = entrada.nextDouble();
+
+        valorPredio = valorInmueble * 0.02;
+
+        mensaje = mensaje + String.format(
+                "El cliente %s con cedula %s tiene un bien inmueble valorado en"
+                + " $%.2f y tiene que pagar de predio $%.2f%n",
+                nombre,
+                cedula,
+                valorInmueble,
+                valorPredio);
+
+        System.out.print(mensaje);
+    }
 }
--- ejercicios/Problemas/src/paquete07/Problema7.java
+++ ejercicios/Problemas/src/paquete07/Problema7.java
@@ -5,10 +5,54 @@
  */
 package paquete07;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema7 {
-    
+
+    public static void main(String[] args) {
+
+        String[] ciudadesEcuador = ciudades();
+
+        System.out.println("Ciudades ingresadas:");
+        for (int i = 0; i < ciudadesEcuador.length; i++) {
+            System.out.println("- " + ciudadesEcuador[i]);
+        }
+
+        System.out.println("\nCiudades con 4 o 5 caracteres:");
+        ciudadesMenoresCinco(ciudadesEcuador);
+
+    }
+
+    public static String[] ciudades() {
+        Scanner entrada = new Scanner(System.in);
+        int numeroCiudades;
+        String nombreCiudades;
+
+        System.out.println("Ingrese el numero de ciudades");
+        numeroCiudades = entrada.nextInt();
+        entrada.nextLine();
+        String[] ciudadesEcuador = new String[numeroCiudades];
+
+        for (int i = 0; i < numeroCiudades; i++) {
+            System.out.println("Ingrese el nombre de la ciudad numero: " + (i + 1));
+            nombreCiudades = entrada.nextLine();
+            ciudadesEcuador[i] = nombreCiudades;
+        }
+
+        return ciudadesEcuador;
+    }
+
+    public static void ciudadesMenoresCinco(String[] ciudadesEcuador) {
+        for (int i = 0; i < ciudadesEcuador.length; i++) {
+
+            if ((ciudadesEcuador[i].length() == 4) || (ciudadesEcuador[i].length() == 5)) {
+                System.out.println(ciudadesEcuador[i]);
+            }
+        }
+    }
+
 }
